11037d922a93611a5c1a6cce964a1b04386cef37
Matheus Oliveira <matheus.euf@gmail.com>
2023-04-08 11:26:07 -0300

    first commit

---
+++ Assets/Scripts/PlacementRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlacementObject))]
+public class PlacementRotation : MonoBehaviour
+{
+    private PlacementObject placementObject;
+    private bool isRotating = false;
+
+    [SerializeField]
+    private Vector3 rotationSpeed = Vector3.zero;
+
+    void Awake()
+    {
+        placementObject = GetComponent<PlacementObject>();
+    }
+
+    void Update()
+    {
+        if(isRotating)
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime, Space.World);
+        }   
+    }
+
+    void OnMouseDown()
+    {
+        isRotating = !isRotating;
+    }
+}
